f620dc21ed69cae3a8d1fa3949e7fabeeb2f257e
tlehoor <thanhletruong21@gmail.com>
2025-01-09 12:35:35 +0700

    add

---
+++ ex4-slot1/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Course names
+        </h1>
+        <ul>
+          <li>React</li>
+          <li>ReactNative</li>
+          <li>NodeJs</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
